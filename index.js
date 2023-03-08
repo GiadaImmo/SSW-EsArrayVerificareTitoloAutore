1afa2223a8367f76508b73befa2e3c2d5efce753
GiadaImmo <120042391+GiadaImmo@users.noreply.github.com>
2023-03-08 10:04:32 +0100

    Lezione_07-03

--- index.js
+++ index.js
@@ -1,6 +1,54 @@
-// Import stylesheets
+/*
+  Partite dall’esercizio precedente introducendo un array contenente oggetti con autore e titolo e ricercando i titoli che corrispondono ad una sequenza di caratteri. Se corrisponde solo un libro visualizzate autore e titolo (ad es. nella console), altrimenti il numero di libri corrispondenti.
+  Come sopra, non utilizzate input box.
+*/
+
 import './style.css';
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+const elencoLibri = [
+  {
+    autore: 'Dante Alighieri',
+    titolo: 'La Divina Commedia',
+  },
+  {
+    autore: 'Alessandro Manzoni',
+    titolo: 'I Promessi Sposi',
+  },
+  {
+    autore: 'Ugo Foscolo',
+    titolo: 'I Sepolcri',
+  },
+  {
+    autore: 'Italo Svevo',
+    titolo: 'Senilità',
+  },
+];
+
+let libroInserito = 'Da'; // Stringa inserita dall'utente per cercare un libro
+let counter = 0;
+
+const libriTrovati = [];
+
+elencoLibri.forEach(trovaLibri);
+
+function trovaLibri(libro) {
+  if (
+    (libro['autore'] + ' ' + libro['titolo'])
+      .toLowerCase()
+      .includes(libroInserito.toLowerCase())
+  ) {
+    libriTrovati.push(libro);
+  }
+}
+
+if (libriTrovati.length == 1) {
+  console.log(
+    libriTrovati[0]['autore'] + ' "' + libriTrovati[0]['titolo'] + '" '
+  );
+} else {
+  if (libriTrovati.length > 1) {
+    console.log(libriTrovati.length);
+  } else {
+    console.log('Nessun libro trovato');
+  }
+}
